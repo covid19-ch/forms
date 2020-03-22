1f0865d35c1a71088942313ac040d3d961216eb3
aposcia <aurelien.poscia@cern.ch>
2020-03-22 21:37:50 +0100

    changed to semicolon as separator

--- script-common.js
+++ script-common.js
@@ -41,7 +41,7 @@ function convertToCSV(objArray) {
     for (var i = 0; i < array.length; i++) {
         var line = '';
         for (var index in array[i]) {
-            if (line != '') line += ','
+            if (line != '') line += ';'
 
             line += array[i][index];
         }
