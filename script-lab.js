685561d80b1d2403e78c1479673478ea388eec2d
Diego Castronuovo <castronu@gmail.com>
2020-03-22 10:33:57 +0100

    Refactor (code simplified)

--- script-lab.js
+++ script-lab.js
@@ -1,5 +1,5 @@
 function setDate(id) {
-    $('#'+id).datepicker({
+    $('#' + id).datepicker({
         changeMonth: true,
         changeYear: true,
         showButtonPanel: true,
@@ -9,7 +9,7 @@ function setDate(id) {
 }
 
 // A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function () {
     setDate('detectionDate');
     setDate('sampleDate');
 });
@@ -17,48 +17,101 @@ $( document ).ready(function() {
 
 function downloadLab() {
 
-   var headers = {
-      detectionDate: 'Detection Date'.replace(/,/g, ''), // remove commas to avoid errors
-      sampleDate: 'Sample Date'.replace(/,/g, ''),
-      nasopharyngealSmear: 'Nasopharyngeal Smear'.replace(/,/g, ''),
-      oralSmear: 'Oral Smear'.replace(/,/g, ''),
-      expectoration: 'Expectoration'.replace(/,/g, ''),
-      endotrachealAspiration: 'Endotracheal Aspiration'.replace(/,/g, ''),
-      bal: 'Brochno-alveolar washing (BAL)'.replace(/,/g, ''),
-      serology: 'Serology'.replace(/,/g, ''),
-      other: 'Other'.replace(/,/g, ''),
-      geneTargetEpositive : 'Gene Target E Positve',
-      geneTargetEnegative : 'Gene Target E Negative',
-      geneTargetRdrpPositive : 'Gene Target RdRp Positive',
-      geneTargetRdrpNegative : 'Gene Target RdRp Negative',
-      geneTargetNpositive : 'Gene Target N Positive',
-      geneTargetNnegative : 'Gene Target N Negative',
-  };
+    var fields = [
+        {
+            domId: 'detectionDate',
+            funcNameForValue: getFormattedValue
+        },
+        {
+            domId: 'sampleDate',
+            funcNameForValue: getFormattedValue
+        },
+        {
+            domId: 'nasopharyngealSmear',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'oralSmear',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'expectoration',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'endotrachealAspiration',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'bal',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'serology',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'other',
+            funcNameForValue: getFormattedValue
+        },
+        {
+            domId: 'geneTargetEpositive',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'geneTargetEnegative',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'geneTargetRdrpPositive',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'geneTargetRdrpNegative',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'geneTargetNpositive',
+            funcNameForValue: getCheckboxValue
+        },
+        {
+            domId: 'geneTargetNnegative',
+            funcNameForValue: getCheckboxValue
+        }
+
+    ]
+
+    var headers = {}
+    var itemsFormatted = {}
+
+    fields.forEach(function (element) {
+        headers[element.domId] = element.domId
+        itemsFormatted[element.domId] = element.funcNameForValue(element.domId)
+    });
+
+    alert(JSON.stringify(itemsFormatted));
+    alert(JSON.stringify(headers));
+
+    /*var headers = {
+       detectionDate: 'Detection Date'.replace(/,/g, ''), // remove commas to avoid errors
+       sampleDate: 'Sample Date'.replace(/,/g, ''),
+       nasopharyngealSmear: 'Nasopharyngeal Smear'.replace(/,/g, ''),
+       oralSmear: 'Oral Smear'.replace(/,/g, ''),
+       expectoration: 'Expectoration'.replace(/,/g, ''),
+       endotrachealAspiration: 'Endotracheal Aspiration'.replace(/,/g, ''),
+       bal: 'Brochno-alveolar washing (BAL)'.replace(/,/g, ''),
+       serology: 'Serology'.replace(/,/g, ''),
+       other: 'Other'.replace(/,/g, ''),
+       geneTargetEpositive : 'Gene Target E Positve',
+       geneTargetEnegative : 'Gene Target E Negative',
+       geneTargetRdrpPositive : 'Gene Target RdRp Positive',
+       geneTargetRdrpNegative : 'Gene Target RdRp Negative',
+       geneTargetNpositive : 'Gene Target N Positive',
+       geneTargetNnegative : 'Gene Target N Negative',
+   };*/
 
 
 
-  itemsFormatted = [
-      {
-          detectionDate: getFormattedValue('detectionDate'),
-          sampleDate: getFormattedValue('sampleDate'),
-          nasopharyngealSmear: getCheckboxValue('nasopharyngealSmear'),
-          oralSmear: getCheckboxValue('oralSmear'),
-          expectoration: getCheckboxValue('expectoration'),
-          endotrachealAspiration: getCheckboxValue('endotrachealAspiration'),
-          bal: getCheckboxValue('bal'),
-          serology: getCheckboxValue('serology'),
-          other: getFormattedValue('other'),
-
-          geneTargetEpositive : getCheckboxValue('geneTargetEpositive'),
-          geneTargetEnegative : getCheckboxValue('geneTargetEnegative'),
-          geneTargetRdrpPositive : getCheckboxValue('geneTargetRdrpPositive'),
-          geneTargetRdrpNegative : getCheckboxValue('geneTargetRdrpNegative'),
-          geneTargetNpositive : getCheckboxValue('geneTargetNpositive'),
-          geneTargetNnegative : getCheckboxValue('geneTargetNnegative'),
-      }
-
-  ];
-
-  var fileTitle = 'test'; // or 'my-unique-title'
-    exportCSVFile(headers, itemsFormatted, fileTitle);
+    var fileTitle = 'test'; // or 'my-unique-title'
+    exportCSVFile(headers, [itemsFormatted], fileTitle);
 }
